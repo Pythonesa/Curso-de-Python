1a58098de7a330ad502f198c67a520bc9965669f
Pythonesa <127327918+Pythonesa@users.noreply.github.com>
2023-04-14 21:52:30 -0300

    add clase 10 pep8

---
+++ code/10_PEP8/notpep.py
@@ -0,0 +1,22 @@
+'''
+def FuncionSuma ( A , B , imprime = True ):
+    resultado=A+B
+    if imprime != False:
+        print(resultado)
+    return resultado
+a      =  5
+var_b  =  3
+FuncionSuma( a , var_b )
+'''
+
+# LOGS = ['logUsuarios.txt', 'logIPs.txt', 'logChat.txt']
+
+'''
+with open(LOGS[0], 'a') as logUsuarios, open(LOGS[1], 'a') as logIPs, open(LOGS[2], 'a') as logChat:
+    pass
+'''
+
+# print("codificación")
+
+# -*- coding: latin-1 -*-
+print("codificación")
